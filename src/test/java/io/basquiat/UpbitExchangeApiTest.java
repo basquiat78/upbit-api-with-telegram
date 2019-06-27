a1c23923fceb81cdb49a0c00cdf37cf706ba7df0
basquiat78 <funnyjazz@naver.com>
2019-06-27 18:09:17 +0900

    테스트 추

--- src/test/java/io/basquiat/UpbitExchangeApiTest.java
+++ src/test/java/io/basquiat/UpbitExchangeApiTest.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import io.basquiat.exchange.domain.Account;
+import io.basquiat.exchange.domain.response.account.Account;
+import io.basquiat.exchange.domain.response.order.OrderChance;
 import io.basquiat.exchange.service.AccountService;
+import io.basquiat.exchange.service.OrderService;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @RunWith(SpringRunner.class)
@@ -18,12 +21,23 @@ public class UpbitExchangeApiTest {
 	@Autowired
 	private AccountService accountService;
 	
-	@Test
+	@Autowired
+	private OrderService orderService;
+	
+	//@Test
 	public void accountAPICallTest() {
 		Flux<Account> flux = accountService.getAccountsWithoutRequestHeader();
 		StepVerifier.create(flux)
 					.expectNextMatches(account -> "BTC".equals(account.getCurrency()))
 					.expectNextCount(4)
+					.verifyComplete();
+	}
+	
+	@Test
+	public void orderChanceAPICallTest() {
+		Mono<OrderChance> mono = orderService.getOrderChanceWithoutRequestHeader("BTC-ETH");
+		StepVerifier.create(mono)
+					.expectNextMatches(oc -> "BTC-ETH".equals(oc.getMarket().getId()))
 					.verifyComplete();
 	}
 	
